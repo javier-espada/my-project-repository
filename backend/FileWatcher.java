3fb42e3ae2fad49d1afe3938bff3426bf88b6e87
javier-espada <javier.espada@qaracter.com>
2025-12-05 09:40:31 +0000

    bugs corrected

--- backend/FileWatcher.java
+++ backend/FileWatcher.java
@@ -1,12 +1,13 @@
+import java.io.IOException;
 import java.nio.file.*;
 
 class FileWatcher{
-    public static void main(String[] args) {
-        path = "../frontend/src/Resources" //Por modificar
+    public static void main(String[] args) throws IOException, InterruptedException{
+        String folderPath = "../frontend/src/Resources"; //Por modificar
 
         WatchService watchService= FileSystems.getDefault().newWatchService();
 
-        Path path = Paths.get(System.getProperty(path)); 
+        Path path = Paths.get(System.getProperty(folderPath)); 
 
         path.register(watchService, StandardWatchEventKinds.ENTRY_CREATE);
 
@@ -14,7 +15,7 @@ class FileWatcher{
         while ((key = watchService.take()) != null) {
             for (WatchEvent<?> event : key.pollEvents()) {
 
-                FileGenerator.generateFile(path+event.context());
+                FileGenerator.generateFile(folderPath+event.context());
                 
             }
             
